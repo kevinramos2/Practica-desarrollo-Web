dc3453b35fa74cafefe57d0f0c2d602034a4bc69
kevinramos2 <keramosl@unal.edu.co>
2024-10-31 22:53:56 -0500

    Create tiposdedatos.js

---
+++ JavaScript/Leccion02/tiposdedatos.js
@@ -0,0 +1,19 @@
+//Tipos de datos en JS
+//String
+var string = "esto es un string"
+//Entero
+var entero = 1
+//Decimal
+var decimal = 1.2
+
+//Tipo de dato Object
+var obj = {
+  nombre: "Juan",
+  apellido: "Perez",
+  telefono: 74852963
+}
+
+console.log(string)
+console.log(entero)
+console.log(decimal)
+console.log(obj)
