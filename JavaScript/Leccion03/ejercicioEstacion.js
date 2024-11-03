3ed9fe4696c3f8d3fad91490ee1301ec285de613
kevinramos2 <keramosl@unal.edu.co>
2024-11-03 13:59:45 -0500

    uso de switch

--- JavaScript/Leccion03/ejercicioEstacion.js
+++ JavaScript/Leccion03/ejercicioEstacion.js
@@ -17,3 +17,26 @@ else{
   console.log("Número de mes incorrecto")
 }
 console.log(estacion)
+
+//Mismo ejercicio pero usando Switch
+
+let numero = 9;
+let estacion2;
+
+switch(numero){
+  case  1: case 2: case 12:
+    estacion2 = "Invierno"
+    break;
+  case 3: case 4: case 5:
+    estacion2 = "Primavera"
+    break;
+  case 6: case 7: case 8:
+    estacion2 = "Verano"
+    break
+  case 9: case 10: case 11:
+    estacion2 = "Otoño"
+    break
+  default:
+    console.log("caso no encontrado")
+}
+console.log(estacion2)
