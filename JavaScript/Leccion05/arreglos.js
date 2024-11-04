2b81596c368f8cd77a6b979a1fc6eaea7f58642a
kevinramos2 <keramosl@unal.edu.co>
2024-11-04 11:17:24 -0500

    Update arreglos.js

--- JavaScript/Leccion05/arreglos.js
+++ JavaScript/Leccion05/arreglos.js
@@ -4,6 +4,9 @@ let autos = new Array(
 );
 //Declar arreglos nuevo []
 const autos2 = ["BMW","Mercedes Benz", "Volvo"];
-for(let i = 0; i<3;i++){
+for(let i = 0; i < autos2.length;i++){
   console.log(autos2[i]);
 }
+
+//Imprimir un dato específico
+console.log(autos2[1])
