ff924d1ba40c2141b76ee4ef84d6609c7647f6d5
kevinramos2 <keramosl@unal.edu.co>
2024-11-05 22:41:47 -0500

    Create gestionPrueba.js

---
+++ JavaScript/POOJS/gestionPrueba.js
@@ -0,0 +1,14 @@
+//Voy a simular un sistema de inventario/gestión de biblioteca simple
+
+class Libro{
+  constructor(titulo,autor,fechaPublicacion,genero){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.fechaPublicacion = fechaPublicacion;
+    this.genero = genero;
+  }
+  //Se va poder mostrar la información del libro
+  mostrarInfo() {
+    
+  }
+}
